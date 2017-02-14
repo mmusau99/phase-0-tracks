72450d8871430754f70024aa2f1e4fb9545c7ce3
mmusau99 <mmusau99@gmail.com>
2017-02-14 13:37:54 -0600

    finished the end of the realease and calmed the beef between vampires and werewolves.

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -3,6 +3,11 @@ def Vampire(str)
 	age_right = true
 	like_garlic_bread = true
 	signup_ins = true
+	count = 0
+	allergies = nil
+puts "How many employees will be processed ?"
+employees = gets.to_i
+ until count == employees do
 
 	puts "What is your name ?"
 	names = gets.chomp
@@ -28,21 +33,33 @@ def Vampire(str)
 	if health_insurance == "No"
 		signup_ins = false
 	end
+	until allergies == "done"
+	puts "Could you name any allergies ? Type 'done' when finished"
+	allergies = gets.chomp
+	end
+
 if (age_right == true && like_garlic_bread == true) || signup_ins == true 
 	puts "Probably not a vampire."
+	count+=1
 
 elsif (age_right == false && like_garlic_bread == false) || signup_ins == false
 	puts "Probably a vampire."
+	count+=1
 
 elsif (age_right == false && like_garlic_bread == false) && signup_ins == false
-	puts "Almost certainly a vampire."
+	puts "Almost certainly a vampire.\n"
+	count+=1
 elsif names == "Drak Cula" || names == "Tu Fang"
 	puts "Definitely a vampire."
+	count+=1
 else
 	puts "Results inconclusive."
+	count+=1
 end
 
 
-
+end
+puts
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
 end
 Vampire("Mumo")
