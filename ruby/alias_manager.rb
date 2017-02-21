db98972ea33613fdc5be6e2d723a95246744f234
mmusau99 <mmusau99@gmail.com>
2017-02-21 12:27:03 -0600

    wrote psuedo code for alias program

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,4 @@
+# write a method that takes in a name as input and creates a fake name
+# swapping the first and last name
+# changing all of the voewls (a,e,i,o, or u) to the next voewl in (aeiou) to the next consonant in the alpabet
+# a -> e and u-> a and d would become 'f'
