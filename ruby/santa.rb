3027c508e83fc42ed55083fbfcab1564c8331532
mmusau99 <mmusau99@gmail.com>
2017-02-27 13:16:52 -0600

    created setter methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,6 @@
 class Santa
    reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet","Cupid", "Donner","Blitzen"]
-   age = 0
+   
 	  
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
@@ -16,10 +16,31 @@ class Santa
 		puts "Initializing Santa instance ..."
 		@gender = gender
 		@ethnicity = ethnicity
+		@age = 0
 
 	end
 	
+	def celebrate_birthday
+	  age+=1
+	end
 	
+	def get_mad_at(name)
+	  @name = name
+	  reindeer_ranking << name
+	end
+	
+	def age
+	  @age
+	end
+	def gender
+	  @gender
+	end
+	def ethnicity
+	  @ethnicity
+  end
+  def gender=(new_gender)
+    @gender = gender
+  end
 end
 santas =[]
 genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A", "gender nonconforming","gender variant"]
@@ -27,3 +48,7 @@ ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to sa
 genders.length.times do |i|
   santas << Santa.new(genders[i], ethnicities[i])
 end
+Mumo = Santa.new("Male","Black")
+Mumo.age
+Mumo.gender = "BLMale"
+Mumo.ethnicity
