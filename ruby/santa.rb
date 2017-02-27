d0d31025b3c97fe50ede19f0704e8bed6d247ddb
mmusau99 <mmusau99@gmail.com>
2017-02-27 11:44:54 -0600

    updated reindeer ranking and age

---
+++ ruby/santa.rb
@@ -0,0 +1,23 @@
+class Santa
+   reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet","Cupid", "Donner","Blitzen"]
+   age = 0
+	  
+	def speak
+		puts "Ho, ho, ho! Haaaappy holidays!"
+
+	end
+
+	def eat_milk_and_cookies(cookie_type)
+		puts "That was a good #{cookie_type}!"
+
+	end
+
+	def initialize(gender, ethnicity)
+		puts "Initializing Santa instance ..."
+		@gender = gender
+		@ethnicity = ethnicity
+
+	end
+	
+	
+end
