2c7133e6c1225b899a79dc987cae6db52b972c18
mmusau99 <mmusau99@gmail.com>
2017-02-27 17:23:58 -0600

    changing length of ethnicities array

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -35,7 +35,7 @@ santas =[]
 genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A", "gender nonconforming","gender variant", "Cisgender Female", "Cisgender Male", "Cisgender Woman", "Female to Male", "Gender Nonconforming", "Gender Questioning", "Non-binary", "Trans Female", "Transgender Male", "Transsexual Male", "Gender Questioning", ]
 ethnicities = ["black", "latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A", "hype-beast", "clown", "Cuban", "Central American", "hispanic", "john legend", "migos", "mongolian", "gerorgian", "sicillian", "dominican", "brazilian", "spanish", "french", "kenyan", "chadaian","ghanaian", "nigerian", "mexican", "bi-racial", "kryptonian", "dark-elf"]
 100.times do 
-  santas << Santa.new(genders[rand(genders.length)], ethnicities[rand(genders.length)])
+  santas << Santa.new(genders[rand(genders.length)], ethnicities[rand(ethnicities.length)])
 end
 Mumo = Santa.new("Male","Black")
 santas.each do |santa|
