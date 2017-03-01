82e241b86dcac57a388d8e0e7c8733c493a2d35b
mmusau99 <mmusau99@gmail.com>
2017-03-01 13:54:24 -0600

    small edit

---
+++ ruby/hash_p.rb
@@ -0,0 +1,18 @@
+# my code should prompt the user for required information and save it within a hash.
+# my code should also use symbols as hashes.
+# user input should be converted to appropriate data type afterwards.
+# Update an existing key, looping not required.
+# if user inputs none skip the loop
+# if the user requires a key update allow for that.
+# make sure to print the latest version of the hash before exiting the program.
+int_Design_A =  {
+	name: " ",
+	address: " ",
+	email: " ",
+	phone: " ",
+	f_shade_blue: " ",
+	wallpaper: ["Paisley", "Chevrons", "Photorealistic woodsy scnenes", "Abstract woodsy scenes"],
+	ombre: ["Fierce", "So last season", "Practically medieval in its appalling irrelevance"]
+}
+
+p int_Design_A
