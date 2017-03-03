fc90ff0708ff5482dfc1a0483684715099f04fe4
mmusau99 <mmusau99@gmail.com>
2017-03-02 22:04:43 -0600

    implemented string count, winning and losing methods, all that is optionall left is prganizing array in a format thats permanenet. I'll most likely try to use the permanent symbol

--- ruby/Word_Game/Guess_Game.rb
+++ ruby/Word_Game/Guess_Game.rb
@@ -6,45 +6,76 @@ one of the letters in the array. if the guess was correct, a message would be di
 and the status of that word would be posted. I'm not yet sure to the extent of how much I should punish errors. 
 maybe 4 mistakes and the other player wins, but ocourse if i do that there needs to be a word length limit. 
 false guesses would also need to return a message with an incorrect statement and the status of the word as well. 
+
+
+Need to use STDN.noecho(&:gets).chomp to hide the word at the beginning of implementation later
   
 =end
 class Word_game
-  attr_reader :selected_word 
-  
+  attr_reader :selected_word,:guess_count, :correct_count,:incorrect_count
   def initialize
-    
+  @incorrect_count
+    @correct_count = 0
     @guess_count = 0
-  
   end
   def word_guess(word)
   @selected_word = word
   end
   
-  
-
-end
-
-Nike = Word_game.new
-
-puts "What word would you like the other player to guess."
-player_word = gets.chomp
-  if player_word.length > 8
-  puts "The word #{player_word} is longer than 8 characters. Please try again"
+  def increment_guess_count
+  @guess_count+=1
   end
-Nike.word_guess(player_word)
+  def increment_correct_count
+    @correct_count+=1
+  end
+  def increment_incorrect_count
+    @incorrect_count+=1
+  end
+  
+end
+player_word = nil
+Player_1 = Word_game.new
+
+  puts "What word would you like the other player to guess."
+  player_word = gets.chomp
+  player_word.upcase!
+  until Player_1.guess_count == player_word.length
+    if player_word.length > 8
+    puts "The word #{player_word} is longer than 8 characters. Please try again"
+    end
+  Player_1.word_guess(player_word)
  
-puts "Next player please guess a letter of the word "
-next_player_guess = gets.chomp
-if next_player_guess.length == 1
-    if Nike.selected_word.include?(next_player_guess)
-      guessing_word = Nike.selected_word
-      index_of_letter = guessing_word.index(next_player_guess)
-      guessing_word = Nike.selected_word.split('').map!{|letter| letter = '_' }
+  puts "Next player please guess a letter of the word "
+  next_player_guess = gets.chomp
+  next_player_guess.capitalize!
+  guessing_word = Player_1.selected_word
+  index_of_letter = guessing_word.index(next_player_guess)
+  guessing_word = Player_1.selected_word.split('').map!{|letter| letter = '_' }
+      
+  if next_player_guess.length == 1
+      if Player_1.selected_word.include?(next_player_guess)
+        
       
     
-    guessing_word[index_of_letter] = next_player_guess
-    puts "#{guessing_word} is within the word"
-    end
-else
-    puts "Sorry try again "
+      guessing_word[index_of_letter] = next_player_guess
+      puts "#{guessing_word} is within the word"
+      Player_1.increment_guess_count
+       Player_1.increment_correct_count
+        if Player_1.correct_count == player_word.length
+          puts "Congratulations you guessed the word!"
+        end
+      else
+      puts "Sorry try again "
+      Player_1.increment_guess_count
+     
+      puts "That is mistake number #{Player_1.incorrect_count}"
+      puts
+      end
+  else
+      puts "Sorry try again "
+      Player_1.increment_guess_count
+  end
+  if Player_1.guess_count == player_word.length
+    puts "Muhahahahahhaha you didn't get it right !?!!??!"
+  end
 end
