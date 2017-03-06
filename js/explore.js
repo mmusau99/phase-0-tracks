14a5a3e16058ed0a2d8ca94a6a1e45518f0ee8c6
mmusau99 <mmusau99@gmail.com>
2017-03-06 13:42:42 -0600

    wrote reverse string method in java

---
+++ js/explore.js
@@ -0,0 +1,21 @@
+/*
+I want to create a function that accepts a string as a parameter.
+this function should take that string and return that string in reverse
+i want to iterate over the string until i've reached the lenght of that string
+I could create a case where the first index is switched with the last one until the word is reversed.
+i could create a counter with the value of the string length and 
+*/
+
+var reverse = function (string){
+  string_copy = string;
+  var string_counter = string.length;
+  string = string.split("");
+  var reverse_string = string;
+  console.log(string);
+  for(i=0; i < string.length; i++){
+    string[i] = string_copy[string_counter-1];
+    string_counter -=1
+  }
+  console.log(string);
+}
+reverse("hello");
