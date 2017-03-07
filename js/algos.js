13f7724fd591fafadf57c45dbec421852d2f05f4
mmusau99 <mmusau99@gmail.com>
2017-03-07 12:56:47 -0600

    added hash creator method and hash match method

--- js/algos.js
+++ js/algos.js
@@ -27,3 +27,32 @@ var long_array = ["long phrase","longest phrase","longer phrase"];
 var long_array_test_1 = ["blue","bluer","bluest","bluerestest", "blugger"];
 longest_String(long_array);
 longest_String(long_array_test_1);
+
+
+
+/*
+i need to create a method that matches key value pairs. I will also need another method to create hashes and match them to objects. 
+to avoid ambiguity I'll set my match method to account for name and ages, too broad a scope won't get me desired results.
+
+*/
+var  create_hash = function (name, age) {
+  var database = {
+    name: name,
+    age: age
+  }
+  return database;
+}
+
+var match_hash = function (hash_1, hash_2){
+  if (hash_1["name"] == hash_2["name"] || hash_1["age"] == hash_2["age"]){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+object_1 = create_hash("mumo", 54);
+object_2 = create_hash("phill", 27);
+
+match_hash(object_1, object_2);
