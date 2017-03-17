f042276c7396d60bf1459f577d12456cbd872563
mmusau99 <mmusau99@gmail.com>
2017-03-17 15:02:41 -0500

    added variable positions

--- Soccer/soccer.rb
+++ Soccer/soccer.rb
@@ -24,12 +24,22 @@ team_rating INT
 );
 SQL
 
+position = ["Striker", "Winger","Central-Midfielder","Defensive-Mid","Central-Attacking-Midfielder", "Center-Back", "Full-Back"]
+
 fantasy_soccer.execute(teams_schema)
 fantasy_soccer.execute(players_schema)
 def create_team (db, team_name, team_rating)
-	fantasy_soccer.execute("INSERT INTO team (team_name, team_rating) VALUES (?, ?)", [team_name, team_rating])
+	db.execute("INSERT INTO team (team_name, team_rating) VALUES (?, ?)", [team_name, team_rating])
 
 end
-
+2. times do
+	create_team(fantasy_soccer, Faker::GameOfThrones.house, Faker::Number.between(1,10))
+	
+end
 def create_players(db,name, number, position, team)
+	db.execute("INSERT INTO players (name, number, age, position) VALUES (?,?,?,?)", [name, number, position, team])
+
 	end
+2. times do
+	create_players(fantasy_soccer,Faker::Number.between(1,11),position[Random.rand(0..8)] )
+end
